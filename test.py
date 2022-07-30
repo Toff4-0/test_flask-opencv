3fdcabf549f9cbd2998cca4532078f057659a2db
Toff4-0 <tof.blondeau@gmail.com>
2022-07-30 20:51:49 +0200

    colect clic data

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 video_stream = VideoStream(src=0).start()
 time.sleep(2.0)
 
+point1 = []
+point2 = []
 
         
 @app.route('/')
@@ -25,6 +27,9 @@ def video_feed():
             frame = video_stream.read()
             output_frame = imutils.resize(frame, width=400)
             output_frame = cv2.flip(output_frame, 1)
+            # draw points if selected
+            cv2.circle()
+            output_frame = cv2.circle(output_frame, center_coordinates, radius, color, thickness)
             # encode the frame in JPEG format
             (flag, encoded_image) = cv2.imencode(".jpg", output_frame)
             yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encoded_image) + b'\r\n')
@@ -34,8 +39,17 @@ def video_feed():
 @app.route('/click_data/<string:data>', methods=['POST'])
 def ProcessClicData(data):
     coord = [ int(x) for x in data.split(',') ]
+    if len(point1) == 2:
+        point2 = coord
+        
+    else:
+        point1 = coord   
+         
     print(coord)
     return('/')
+
+
+
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
